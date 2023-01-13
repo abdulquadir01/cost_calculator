4017b8d5a457514363d4a524cecc2ff9b4455f3d
AbubakrRehman <abubakr.rehman91@outlook.com>
2023-01-13 19:15:33 +0530

    redux initialised with products as state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from "axios";
+import {useReducer,useState,useEffect} from "react";
+import { cartReducer } from './reducers/cartReducer';
 
 function App() {
+
+  const [state, dispatch] = useReducer(cartReducer,{
+    cart:[],
+    products:[]
+  });
+  console.log(state);
+  const fetchProducts=async()=>{
+    const {data}=await axios.get('https://dummyjson.com/products');
+    //console.log(data);
+    dispatch({
+      type:"ADD_PRODUCTS",
+      payload:data.products
+    })
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  
+    
+  }, [])
+  
+ 
+
   return (
    <h1>Hello</h1>
   );
